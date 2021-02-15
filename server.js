410ee33398c06ebb23be0e38181dc35ed140ceb6
itsnotnehav <66019061+itsnotnehav@users.noreply.github.com>
2021-02-15 15:29:51 +0530

    Added comments

--- server.js
+++ server.js
@@ -30,20 +30,20 @@ app.use(express.json());
 app.post('/api', (req, res) => {
     forChatbot.connect("mongodb://localhost:27017/", async function(err, db) {
         if (err) throw err
-        const ques1 = req.body
+        const ques1 = req.body //request from frontend (message entered by user)
         console.log(ques1)
         console.log("Entering into the database");
         const dbo = db.db("chatbot_app_db")
-        const result = await dbo.collection("dialogues").find(ques1).toArray()
+        const result = await dbo.collection("dialogues").find(ques1).toArray() //searching for the user entered message in database.
         console.log("Result from database")
-        console.log(result)
+        console.log(result) //resultant record in an array. If the user entered message is not in the database, result will be an empty array. 
 
-        if (result.length === 0 ) {
+        if (result.length === 0 ) { //User entered message not found in database.
             return res.json({status : 'error', error : "Sorry, I don't understand"})
-        } else {
+        } else { //User entered message found in database.
             console.log(result[0].response)
-            const reply = result[0].response 
-            res.json({
+            const reply = result[0].response //reply message of the chatbot.
+            res.json({ //response from backend to frontend.
                 status : 'okay',
                 ans : reply
             })
@@ -52,22 +52,22 @@ app.post('/api', (req, res) => {
 })
 
 app.post('/api/login', async (req, res) => {
-    const {username, password} = req.body
-    const user = await User.findOne({username}).lean()
-    if(!user) {
+    const {username, password} = req.body //Data from the request sent by the frontend (username and password entered by the user).
+    const user = await User.findOne({username}).lean() //Searching if the username entered, exists in the database.
+    if(!user) { // If doesn't exist
         return res.json({status : 'error', error : 'Invalid username / password'})
     }
-    if(await bcrypt.compare(password, user.password)) {
+    if(await bcrypt.compare(password, user.password)) { // If user - entered password matches with the encrypted password in the db, generate the token.
         const token = jwt.sign({id : user._id, username : user.username}, JWT_SECRET) //public and visible.
-        return res.json({status : 'ok',  data : token})
+        return res.json({status : 'ok',  data : token}) // Log-in 
     }
 
     res.json({status : 'error', error : 'Invalid username / password'})
 })
 
 app.post('/api/register', async (req, res) => {
-    const {username, password : plainTextPassword} = req.body
-    if(!username || typeof username !== 'string') {
+    const {username, password : plainTextPassword} = req.body // Data sent by the front end. (Username and password entered by the user)
+    if(!username || typeof username !== 'string') { 
         return res.json({status : 'error', error : 'Invalid username'})
     }
     if(!plainTextPassword || typeof plainTextPassword !== 'string') {
@@ -79,16 +79,16 @@ app.post('/api/register', async (req, res) => {
         })
     }
     
-    const password = await bcrypt.hash(plainTextPassword, 10)
+    const password = await bcrypt.hash(plainTextPassword, 10) // encrypting the user entered password.
     try {
-        const response = await User.create({
+        const response = await User.create({ // Entering the username and encrypted password into the database.
             username,
             password
         })
         console.log('User created Successfully : ', response)        
 
     } catch(error) {     
-        if (error.code === 11000) {
+        if (error.code === 11000) { // If user enters username that already exists. (Code 11000 indicates the duplicates)
             return res.json({status : 'error', error : 'Username already in use'})
         }
         throw error
